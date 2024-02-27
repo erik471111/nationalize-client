f4060cadb8cf59a84ffffae43b465a0c1bf4ee85
Szász Hanna Rebeka <szasz.hanna.rebeka@gmail.com>
2024-02-27 14:50:01 +0100

    build: delete the now unused commons-lang3 dependency from pom.xml

--- src/main/java/nationalize/Nationality.java
+++ src/main/java/nationalize/Nationality.java
@@ -6,23 +6,16 @@ import java.util.List;
 @lombok.Data
 public class Nationality {
 
-
     private long count;
     private String name;
 
     @SerializedName("country")
     private List<Country> countries;
 
-
-
     @lombok.Data
     public static class Country {
 
         private String countryId;
         private float probability;
-
-
-
     }
-
 }
